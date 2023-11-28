44521a4d9c5b6f241aa45f52412b168ebeaba4aa
Cameron Hyde <chyde@neoformit.com>
2023-11-28 13:56:43 +1000

    Fix docker build script

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t neoformit/ena-upload:v0.1 . && docker push neoformit/ena-upload:v0.1
+docker build -t neoformit/ena-upload:v1.0 . && docker push neoformit/ena-upload:v1.0
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ WDIR=/app
 
 # Can remove stdout/err redirect for debugging, otherwise shows Flask/Vue logs
 cd $WDIR
-honcho start -f Procfile-docker #> /dev/null 2>&1 &
+honcho start -f Procfile-docker > /dev/null 2>&1 &
 
 while [[ ! -f "$ENA_OUTPUT_STUDY" ]]; do
     sleep 0.25
